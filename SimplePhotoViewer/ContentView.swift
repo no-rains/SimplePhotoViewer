a8dd2ce8dd28468a9f73da020e10aef0d7d84161
norains <norains@norainsdeMacBook-Pro.local>
2020-03-17 22:26:54 +0800

    Add the base source code to show the photo

---
+++ SimplePhotoViewer/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  TestImageView
+//
+//  Created by norains on 2020/3/17.
+//  Copyright © 2020 norains. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var sourceRect: CGRect = .zero
+    @State var selectedImage: ImageData = ImageData()
+
+    let imageViewerAnimatorBindings = ImageViewerAnimatorBindings()
+
+    var body: some View {
+        return ZStack(alignment: .topLeading) {
+            ImageView(sourceRect: $sourceRect, selectedImage: $selectedImage, imageName: "photo", height: 200, cornerRadius: 20)
+                .padding()
+                .environmentObject(imageViewerAnimatorBindings)
+
+            ImageViewAnimator(sourceRect: sourceRect, selectedImage: selectedImage)
+                .environmentObject(imageViewerAnimatorBindings)
+        }
+        .environmentObject(imageViewerAnimatorBindings)
+        .coordinateSpace(name: "globalCooardinate")
+    }
+}
