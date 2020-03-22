d489cf20a40db6ea08974d8f9b98482e49dfc503
norains <norains@norainsdeMacBook-Pro.local>
2020-03-22 08:41:14 +0800

    Set some property as private

--- SimplePhotoViewer/PhotoViewer.swift
+++ SimplePhotoViewer/PhotoViewer.swift
@@ -19,24 +19,27 @@ struct PhotoViewer: View {
 }
 
 fileprivate struct ImageWrapper: View {
-    // The image name
-    let image: UIImage
-
-    // The frame for the image view
-    let frame: CGRect
-
     // The actual scale base on the minSize
     @State var actualScale: CGFloat = 1.0
 
     // The actual offset base on the minPosition
     @State var actualOffset: CGPoint = .zero
 
-    var minImgSize: CGSize
-    var maxImgSize: CGSize
-    var minImgDisplayPoint: CGPoint
-    var minScale: CGFloat
-    var maxScale: CGFloat
-    @State var scaleRatio: CGFloat = 1 // Base on the minScale
+    // Base on the minScale, the min value is 1
+    @State var scaleRatio: CGFloat = 1
+
+    // The image
+    private let image: UIImage
+
+    // The frame for the image view
+    private let frame: CGRect
+
+    // The value is for draging and zoom gesture
+    private let minImgSize: CGSize
+    private let maxImgSize: CGSize
+    private let minImgDisplayPoint: CGPoint
+    private let minScale: CGFloat
+    private let maxScale: CGFloat
 
     init(image: UIImage, frame: CGRect) {
         self.image = image
@@ -62,10 +65,10 @@ fileprivate struct ImageWrapper: View {
         print("image size:\(image.cgImage!.width),\(image.cgImage!.height)")
         print("screen:\(frame)")
         print("minImgPoint:\(minImgDisplayPoint)")
-        // print("actualSize:\(actualSize.width)")
     }
 
-    @State var actualSize: CGSize = .zero
+    @State private var lastTranslation: CGSize?
+    @State private var lastScale: CGFloat?
 
     // Magnify and Rotate States
     @State private var magScale: CGFloat = 1
@@ -74,12 +77,6 @@ fileprivate struct ImageWrapper: View {
 
     // Drag Gesture Binding
     @State private var dragOffset: CGSize = .zero
-
-    // The actual position and size after scaling and being offset
-    @State private var actualPosition: CGPoint = .zero
-
-    @State private var lastTranslation: CGSize?
-    @State private var lastScale: CGFloat?
 
     var body: some View {
         let rotateAndZoom = MagnificationGesture()
@@ -118,40 +115,21 @@ fileprivate struct ImageWrapper: View {
                 self.dragOffset = value.translation
 
                 if let lastTranslation = self.lastTranslation {
-                    self.actualPosition.x += value.translation.width - lastTranslation.width
-                    self.actualPosition.y += value.translation.height - lastTranslation.height
                 }
 
                 self.lastTranslation = value.translation
-
-                // self.actualPosition.x += value.translation.width
-                // self.actualPosition.y += value.translation.height
             }
             .onEnded { value in
                 if self.isScaled {
                     self.dragOffset = value.translation
 
                     if let lastTranslation = self.lastTranslation {
-                        self.actualPosition.x += value.translation.width - lastTranslation.width
-                        self.actualPosition.y += value.translation.height - lastTranslation.height
                     }
 
                     self.lastTranslation = nil
 
-                    if self.actualSize.width <= self.frame.width {
-                        self.actualPosition.x = 0
-                    }
-
-                    if self.actualSize.height <= self.frame.height {
-                        self.actualPosition.y = 0
-                    }
-
                 } else {
                     self.dragOffset = CGSize.zero
-
-                    self.actualPosition = .zero
-                    self.actualSize.width = self.frame.width
-                    self.actualSize.height = self.frame.height
                 }
             }
 
@@ -173,9 +151,8 @@ fileprivate struct ImageWrapper: View {
             .gesture(fitToFill)
             // .scaleEffect(isScaled ? magScale : max(1 - abs(self.dragOffset.height) * 0.004, 0.6), anchor: .center)
             // .offset(x: dragOffset.width * magScale, y: dragOffset.height * magScale)
-            // .offset(x: actualPosition.x, y: actualPosition.y)
             .scaleEffect(minScale * scaleRatio, anchor: .center)
-            // .offset(x: minImgPoint.x, y:minImgPoint.y)
+            .offset(x: actualOffset.x, y: actualOffset.y)
             .animation(.spring(response: 0.4, dampingFraction: 0.9))
     }
 }
